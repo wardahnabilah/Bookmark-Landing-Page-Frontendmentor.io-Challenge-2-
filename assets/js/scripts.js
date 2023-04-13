7e731fe4efe0b2e6e507facf625eb2a153cd3e67
unknown <wardahnabilah33@gmail.com>
2023-04-13 11:46:46 +0700

    Add styles and interaction for features section (mobile and desktop)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,7 @@
+/*******************************************************************
+                            HEADER SECTION
+********************************************************************/
+
 /* Showing and hiding navigation */
 
 const toggle = document.querySelector(".toggle")
@@ -14,3 +18,29 @@ function toggleNavigation() {
 
     console.log(navComponent);
 }
+
+/*******************************************************************
+                            FEATURES SECTION
+********************************************************************/
+
+const featuresTab = document.querySelectorAll(".features__item")
+
+featuresTab.forEach(tab => {
+    // Add event listener 'click' to every tab
+    tab.addEventListener("click", () => {
+        const currentTab = tab.dataset.featuresTab //which tab is being selected (using data attribute 'data-features-tab')
+        const allTabElements = document.querySelectorAll("[data-features-tab]") //Select all feature tab elements
+
+        // Loop through all feature tab elements
+        allTabElements.forEach( item => {
+            // If the element matches currentTab, add 'active' class
+            if (item.matches(`[data-features-tab='${currentTab}']`)) { 
+                item.classList.add("active")
+            } 
+            // If doesn't match, remove 'active' class
+            else {
+                item.classList.remove("active")
+            }
+        })
+    })
+})
